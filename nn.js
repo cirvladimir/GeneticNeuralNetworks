6c74267cd83ce58739007c7952794c11c8e432c0
vovakkk <vladimirkorukov@gmail.com>
2015-10-18 00:58:26 -0700

    Added runNetwork.

--- nn.js
+++ nn.js
@@ -40,9 +40,12 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
 
 var generateNetwork = function (nLayers, nInput, nOutput) {
-    var nn = { neurons: [], input: [], output: [] };
+    var nn = { neurons: [], input: [], output: [], layers: [] };
+    var genNeuron = function() {
+        return { bias: Math.random() * 2 - 1, connections: [], value: 0 };
+    };
     var front = Array.range(nInput).map(function() {
-        return { bias: Math.random(), connections: [] };
+        return genNeuron();
     });
     nn.input = front;
     nn.neurons = nn.neurons.concat(front);
@@ -64,29 +67,31 @@ var generateNetwork = function (nLayers, nInput, nOutput) {
         var newFront = [];
         var used = Array.range(front.length);
         while (!used.every(_)) {
-            var newNeuron = { bias: Math.random(), connections: [] };
+            var newNeuron = genNeuron();
             newFront.push(newNeuron);
             nn.neurons.push(newNeuron);
             $(distinctRandom, Math.random() * front.length / 4 * 3 + front.length / 8, front.length).forEach(function (ind) {
-                newNeuron.connections.push(front[ind]);
+                newNeuron.connections.push({weight: Math.random(), neuron: front[ind]});
                 used[ind] = true;
             });
         }
+        nn.layers.push(newFront.map(_));
         $(distinctRandom, Math.random() * front.length / 3, front.length).forEach(function (ind) {
             newFront.push(front[ind]);
         });
         front = newFront;
     });
     nn.output = Array.range(nOutput).map(function () {
-        return { bias: Math.random(), connections: [] };
+        return genNeuron();
     });
+    nn.layers.push(nn.output);
     nn.neurons = nn.neurons.concat(nn.output);
     front.forEach(function (el, ind) {
         Array.range(Math.max(1, Math.floor(nOutput / 4))).forEach(function () {
             var randOut = Math.floor(Math.random() * nOutput);
             while (nn.output[randOut].connections.some($(ФEq, el)))
                 randOut = Math.floor(Math.random() * nOutput);
-            nn.output[randOut].connections.push(el);
+            nn.output[randOut].connections.push({weight: Math.random(), neuron: el});
         });
     });
     nn.output.forEach(function (el, ind) {
@@ -94,11 +99,27 @@ var generateNetwork = function (nLayers, nInput, nOutput) {
             var randIn = Math.floor(Math.random() * front.length);
             while (el.connections.some($(ФEq, front[randIn])))
                 randIn = Math.floor(Math.random() * front.length);
-            el.connections.push(front[randIn]);
+            el.connections.push({weight: Math.random(), neuron: front[randIn]});
         });
     });
     return nn;
 };
 
+var σ = function(z) {
+    return 1 / (1 + Math.exp(-z));
+};
 
+var runNetwork = function (nn, input) {
+    input.forEach(function (v, i) {
+        nn.input[i].value = v;
+    });
+    nn.layers.forEach(function (layer) {
+        layer.forEach(function (neuron) {
+            neuron.value = σ(neuron.connections.reduce(function (acc, con) {
+                return acc + con.weight * con.neuron.value;
+            }, neuron.bias));
+        });
+    });
+    return nn.output.map(function(neuron) { return neuron.value; });
+};
 
