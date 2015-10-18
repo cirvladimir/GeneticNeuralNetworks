5f5e9f132aacb807466116652b733fb1d2e1e25b
Francis Ge <fge3@uw.edu>
2015-10-18 16:53:33 -0700

    Refactored out Perlin Noise code and Neuron Geometry generation. Example of usage in test.html.

---
+++ test/libs/neuron_geo.js
@@ -0,0 +1,187 @@
+var generate_neuron_geo = function(neighbors) {
+	var X = 0.525731112119133606;
+	var Z = 0.850650808352039932;
+	var p_noise = new pnoise();
+
+	var icosahedron_verts = [    
+		-X, 0.0, Z, 
+		X, 0.0, Z, 
+		-X, 0.0, -Z, 
+		X, 0.0, -Z,    
+		0.0, Z, X, 
+		0.0, Z, -X, 
+		0.0, -Z, X, 
+		0.0, -Z, -X,    
+		Z, X, 0.0, 
+		-Z, X, 0.0, 
+		Z, -X, 0.0, 
+		-Z, -X, 0.0
+	];
+
+	var icosahedron_faces = [
+		0,4,1, 
+		0,9,4, 
+		9,5,4, 
+		4,5,8, 
+		4,8,1,    
+		8,10,1, 
+		8,3,10, 
+		5,3,8, 
+		5,2,3, 
+		2,7,3,    
+		7,10,3, 
+		7,6,10, 
+		7,11,6, 
+		11,0,6, 
+		0,1,6, 
+		6,1,10, 
+		9,0,11,
+		9,11,2,
+		9,2,5, 
+		7,2,11
+	];
+
+	var UVs = [];
+
+	// For each face, there's three vertices, I want to subdivide these vertices and make new faces.
+	// If I subdivide again, I will subdivide these new faces.
+	function subdivide(verts, faces, subdivisions) {
+		if (subdivisions == 0) return faces;
+		// After these vertices are generated, add them to the global array.
+		// Then use the indices into the array to create the faces.
+		var new_faces = [];
+		for (var i = 0; i < faces.length; i += 3) {
+			var v1 = [verts[faces[i] * 3], verts[faces[i] * 3 + 1], verts[faces[i] * 3 + 2]];
+			var v2 = [verts[faces[i + 1] * 3], verts[faces[i + 1] * 3 + 1], verts[faces[i + 1] * 3 + 2]];
+			var v3 = [verts[faces[i + 2] * 3], verts[faces[i + 2] * 3 + 1], verts[faces[i + 2] * 3 + 2]];
+			var v1_2 = [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]];
+			var v2_3 = [v2[0] + v3[0], v2[1] + v3[1], v2[2] + v3[2]];
+			var v3_1 = [v3[0] + v1[0], v3[1] + v1[1], v3[2] + v1[2]];
+			v1_2 = normalize(v1_2);
+			v2_3 = normalize(v2_3);
+			v3_1 = normalize(v3_1);
+			// Add the vertices
+			var index_v1_2 = verts.length / 3;
+			verts.push(v1_2[0]);
+			verts.push(v1_2[1]);
+			verts.push(v1_2[2]);
+			var index_v2_3 = verts.length / 3;
+			verts.push(v2_3[0]);
+			verts.push(v2_3[1]);
+			verts.push(v2_3[2]);
+			var index_v3_1 = verts.length / 3;
+			verts.push(v3_1[0]);
+			verts.push(v3_1[1]);
+			verts.push(v3_1[2]);
+			// Add the faces
+			new_faces.push(faces[i], index_v1_2, index_v3_1);
+			new_faces.push(faces[i + 1], index_v2_3, index_v1_2);
+			new_faces.push(faces[i + 2], index_v3_1, index_v2_3);
+			new_faces.push(index_v1_2, index_v2_3, index_v3_1);
+		}
+		return subdivide(verts, new_faces, subdivisions - 1);
+	}
+
+	function computeSphereUVs(verts) {
+		var UVs = [];
+		for (var i = 0; i < verts.length; i += 3) {
+			var d = [0 - verts[i], 0 - verts[i + 1], verts[i + 2]]; // This way it is shaded consistently with the regular sphere
+			d = normalize(d);
+			var u = 0.5 + Math.atan2(d[2], d[0]) / (2 * Math.PI);
+			var v = 0.5 - Math.asin(d[1]) / Math.PI;
+			UVs.push(u);
+			UVs.push(v);
+		}
+		return UVs;
+	}
+
+	function normalize(v) {
+		var mag = magnitude(v);
+		return [v[0] / mag, v[1] / mag, v[2] / mag];
+	}
+
+	function magnitude(v) {
+		return Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+	}
+
+	function dotProduct(u, v) {
+		return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+	}
+
+	function sub(u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] - v[i];
+		return vect;	
+	}
+
+	function mult(v, k) {
+		var vect = [];
+		for (var i = 0; i < v.length; i++)
+			vect[i] = v[i] * k;
+		return vect;	
+	}
+
+	icosahedron_faces = subdivide(icosahedron_verts, icosahedron_faces, 3);
+	UVs = computeSphereUVs(icosahedron_verts);
+
+	/// Example of adding edges
+	neighbors = {};
+	neighbors.vectors = [];
+	neighbors.vectors.push([-100, 0, 0]);
+	neighbors.vectors.push([300, 0, 0]);
+	neighbors.vectors.push([0, 100, 0]);
+	neighbors.vectors.push([0, -100, 0]);
+	neighbors.vectors.push([0, 0, 200]);
+	neighbors.vectors.push([0, 0, -200]);
+	var vectors_to_neighbors = neighbors.vectors;
+
+	// Faces are not in the right CCW order - so we need to account for that
+	var icosahedron_geo = new THREE.Geometry();
+	for (var i = 0; i < icosahedron_verts.length; i += 3) {
+		var position = [icosahedron_verts[i] * 20, icosahedron_verts[i+1] * 20, icosahedron_verts[i+2] * 20];
+		var normal = normalize(position);
+
+	    var newPosition = p_noise.deformPoint(position, normal);
+	    // var newPosition = p_noise.deformPointTime(position, normal);
+
+		// Edge attraction
+		for (var j = 0; j < vectors_to_neighbors.length; j++) {
+			var vector_to_neighbor_unnormalized = vectors_to_neighbors[j];
+			var distance_to_neighbor = magnitude(vector_to_neighbor_unnormalized);
+			var vector_to_neighbor_normalized = normalize(vector_to_neighbor_unnormalized);
+
+			var vertex_to_neighbor = sub(mult(vector_to_neighbor_normalized, 20), position);
+			var test_vector = normalize(sub(vector_to_neighbor_unnormalized, position));
+			test_vector = vector_to_neighbor_normalized;
+			var angle = Math.acos(dotProduct(normalize(vertex_to_neighbor), vector_to_neighbor_normalized)) * 180 / Math.PI;
+			if (angle > 85.0) {
+				var scale = (90.0 - angle) / 5.0;
+				newPosition[0] = newPosition[0] + test_vector[0] * distance_to_neighbor * scale;
+				newPosition[1] = newPosition[1] + test_vector[1] * distance_to_neighbor * scale;
+				newPosition[2] = newPosition[2] + test_vector[2] * distance_to_neighbor * scale;
+			}
+		}
+
+		icosahedron_geo.vertices.push(new THREE.Vector3(newPosition[0], newPosition[1], newPosition[2]));
+	}
+
+	for (var i = 0; i < icosahedron_faces.length; i += 3) {
+		icosahedron_geo.faces.push(new THREE.Face3(icosahedron_faces[i], icosahedron_faces[i+2], icosahedron_faces[i+1]));
+	}
+
+	for (var i = 0; i < icosahedron_faces.length; i += 3) {
+		var faceUVs = [
+			new THREE.Vector2(UVs[icosahedron_faces[i] * 2], UVs[icosahedron_faces[i] * 2 + 1]),
+			new THREE.Vector2(UVs[icosahedron_faces[i + 2] * 2], UVs[icosahedron_faces[i + 2] * 2 + 1]),
+			new THREE.Vector2(UVs[icosahedron_faces[i + 1] * 2], UVs[icosahedron_faces[i + 1] * 2 + 1])
+		];
+		icosahedron_geo.faceVertexUvs[0].push(faceUVs);
+	}
+
+	icosahedron_geo.mergeVertices();
+	icosahedron_geo.computeFaceNormals();
+	icosahedron_geo.computeVertexNormals();
+
+	return icosahedron_geo;
+}
--- test/libs/pnoise.js
+++ test/libs/pnoise.js
@@ -11,191 +11,225 @@
 // https://github.com/ashima/webgl-noise
 //
 
-// Takes in a vec3 or vec4
-function mod289(x) {
-	var vect = [];
-	for (var i = 0; i < x.length; i++)
-		vect[i] = x[i] - Math.floor(x[i] * (1.0 / 289.0)) * 289.0;
-	return vect;
-}
-
-// Takes in a vec4
-function permute(x)
-{
-	var vect = [];
-	for (var i = 0; i < x.length; i++)
-		vect[i] = ((x[i] * 34.0) + 1.0) * x;
-	return mod289(vect);
-}
-
-// Takes in a return vec4
-function taylorInvSqrt(r)
-{
-	var vect = [];
-	for (var i = 0; i < r.length; i++)
-		vect[i] = 1.79284291400159 - 0.85373472095314 * r[i];
-	return vect;
-}
-
-// Takes in a vec3
-function fade(t) {
-	var vect = [];
-	for (var i = 0; i < t.length; i++)
-		vect[i] = t[i] * t[i] * t[i] * (t[i] * (t[i] * 6.0 - 15.0) + 10.0)
-	return vect;
-}
-
-// function multiply(u, v) {
-// 	return [u[0] * v[0], u[1] * v[1], u[2] * v[2]];
-// }
-
-function add(u, v) {
-	var vect = [];
-	for (var i = 0; i < u.length; i++)
-		vect[i] = u[i] + v[i];
-	return vect;	
-}
-
-function sub(u, v) {
-	var vect = [];
-	for (var i = 0; i < u.length; i++)
-		vect[i] = u[i] - v[i];
-	return vect;	
-}
-
-function mod (u, v) {
-	var vect = [];
-	for (var i = 0; i < u.length; i++)
-		vect[i] = u[i] % v[i];
-	return vect;
-}
-
-function floor(v) {
-	var vect = [];
-	for (var i = 0; i < v.length; i++)
-		vect[i] = Math.floor(v[i]);
-	return vect;
-}
-
-function fract(v) {
-	var vect = [];
-	for (var i = 0; i < v.length; i++)
-		vect[i] = v[i] - Math.floor(v[i]);
-	return vect;
-}
-
-function mult(v, k) {
-	var vect = [];
-	for (var i = 0; i < v.length; i++)
-		vect[i] = v[i] * k;
-	return vect;	
-}
-
-function multiply(u, v) {
-	var vect = [];
-	for (var i = 0; i < u.length; i++)
-		vect[i] = u[i] * v[i];
-	return vect;	
-}
-
-function abs(v) {
-	var vect = [];
-	for (var i = 0; i < v.length; i++)
-		vect[i] = Math.abs(v[i]);
-	return vect;
-}
-
-function step(u, v) {
-	var vect = [];
-	for (var i = 0; i < u.length; i++)
-		vect[i] = u[0] < v[0] ? 0 : 1;
-	return vect;
-}
-
-// Classic Perlin noise, periodic variant
-// Takes vec3 P, vec3 
-// Returns float
-function pnoise(P, rep) {
-	var Pi0 = mod(floor(P), rep); // Integer part, modulo period
-	var Pi1 = mod(add(Pi0, [1.0, 1.0, 1.0]), rep); // Integer part + 1, mod period
-	Pi0 = mod289(Pi0);
-	Pi1 = mod289(Pi1);
-	var Pf0 = fract(P); // Fractional part for interpolation
-	var Pf1 = sub(Pf0, [1.0, 1.0, 1.0]); // Fractional part - 1.0
-
-	var ix = [Pi0[0], Pi1[0], Pi0[0], Pi1[0]];
-	var iy = [Pi0[1], Pi0[1], Pi1[1], Pi1[1]];
-	var iz0 = [Pi0[2], Pi0[2], Pi0[2], Pi0[2]];
-	var iz1 = [Pi1[2], Pi1[2], Pi1[2], Pi1[2]];
-
-	var ixy = permute(add(permute(ix), iy));
-	var ixy0 = permute(add(ixy, iz0));
-	var ixy1 = permute(add(ixy, iz1));
-
-	var gx0 = mult(ixy0, 1.0 / 7.0);
-	var gy0 = sub(fract(mult(floor(gx0), 1.0 / 7.0)), [0.5, 0.5, 0.5, 0.5]);
-	gx0 = fract(gx0);
-	var gz0 = sub(sub([0.5, 0.5, 0.5, 0.5], abs(gx0)), abs(gy0));
-	var sz0 = step(gz0, [0, 0, 0, 0]);
-	gx0 -= multiply(sz0, sub(step([0, 0, 0, 0], gx0), [0.5, 0.5, 0.5, 0.5]));
-	gy0 -= multiply(sz0, sub(step([0, 0, 0, 0], gy0), [0.5, 0.5, 0.5, 0.5]));
-
-	var gx1 = mult(ixy1, 1.0 / 7.0);
-	var gy1 = sub(fract(mult(floor(gx1), 1.0 / 7.0)), [0.5, 0.5, 0.5, 0.5]);
-	gx1 = fract(gx1);
-	var gz1 = sub(sub([0.5, 0.5, 0.5, 0.5], abs(gx1)), [0.5, 0.5, 0.5, 0.5]);
-	var sz1 = step(gz1, [0.0, 0.0, 0.0, 0.0]);
-
-	vec4 gx1 = ixy1 * (1.0 / 7.0);
-	vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
-	gx1 = fract(gx1);
-	vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-	vec4 sz1 = step(gz1, vec4(0.0));
-	gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-	gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-
-	vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-	vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-	vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-	vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-	vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-	vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-	vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-	vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-
-	vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-	g000 *= norm0.x;
-	g010 *= norm0.y;
-	g100 *= norm0.z;
-	g110 *= norm0.w;
-	vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-	g001 *= norm1.x;
-	g011 *= norm1.y;
-	g101 *= norm1.z;
-	g111 *= norm1.w;
-
-	float n000 = dot(g000, Pf0);
-	float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-	float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-	float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-	float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-	float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-	float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-	float n111 = dot(g111, Pf1);
-
-	vec3 fade_xyz = fade(Pf0);
-	vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-	vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-	float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
-	return 2.2 * n_xyz;
+var pnoise = function() {
+	// Takes in a vec3 or vec4
+	function mod289(x) {
+		var vect = [];
+		for (var i = 0; i < x.length; i++)
+			vect[i] = x[i] - Math.floor(x[i] * (1.0 / 289.0)) * 289.0;
+		return vect;
 	}
 
-	float turbulence( vec3 p ) {
-	float w = 100.0;
-	float t = -.5;
-	for (float f = 1.0 ; f <= 10.0 ; f++ ){
-	float power = pow( 2.0, f );
-	t += abs( pnoise( vec3( power * p ), vec3( 10.0, 10.0, 10.0 ) ) / power );
+	// Takes in a vec4
+	function permute(x)
+	{
+		var vect = [];
+		for (var i = 0; i < x.length; i++)
+			vect[i] = ((x[i] * 34.0) + 1.0) * x[i];
+		return mod289(vect);
 	}
-	return t;
+
+	// Takes in a return vec4
+	function taylorInvSqrt(r)
+	{
+		var vect = [];
+		for (var i = 0; i < r.length; i++)
+			vect[i] = 1.79284291400159 - 0.85373472095314 * r[i];
+		return vect;
+	}
+
+	// Takes in a vec3
+	function fade(t) {
+		var vect = [];
+		for (var i = 0; i < t.length; i++)
+			vect[i] = t[i] * t[i] * t[i] * (t[i] * (t[i] * 6.0 - 15.0) + 10.0)
+		return vect;
+	}
+
+	// function multiply(u, v) {
+	// 	return [u[0] * v[0], u[1] * v[1], u[2] * v[2]];
+	// }
+
+	function add(u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] + v[i];
+		return vect;	
+	}
+
+	function sub(u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] - v[i];
+		return vect;	
+	}
+
+	function mod (u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] % v[i];
+		return vect;
+	}
+
+	function floor(v) {
+		var vect = [];
+		for (var i = 0; i < v.length; i++)
+			vect[i] = Math.floor(v[i]);
+		return vect;
+	}
+
+	function fract(v) {
+		var vect = [];
+		for (var i = 0; i < v.length; i++)
+			vect[i] = v[i] - Math.floor(v[i]);
+		return vect;
+	}
+
+	function mult(v, k) {
+		var vect = [];
+		for (var i = 0; i < v.length; i++)
+			vect[i] = v[i] * k;
+		return vect;	
+	}
+
+	function multiply(u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] * v[i];
+		return vect;	
+	}
+
+	function abs(v) {
+		var vect = [];
+		for (var i = 0; i < v.length; i++)
+			vect[i] = Math.abs(v[i]);
+		return vect;
+	}
+
+	function step(u, v) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] < v[i] ? 0 : 1;
+		return vect;
+	}
+
+	function dot(u, v) {
+		return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+	}
+
+	function mix(u, v, a) {
+		var vect = [];
+		for (var i = 0; i < u.length; i++)
+			vect[i] = u[i] * (1 - a) + v[i] * a;
+		return vect;
+	}
+
+	// Classic Perlin noise, periodic variant
+	// Takes vec3 P, vec3 
+	// Returns float
+	function pnoise(P, rep) {
+		var Pi0 = mod(floor(P), rep); // Integer part, modulo period
+		var Pi1 = mod(add(Pi0, [1.0, 1.0, 1.0]), rep); // Integer part + 1, mod period
+		Pi0 = mod289(Pi0);
+		Pi1 = mod289(Pi1);
+		var Pf0 = fract(P); // Fractional part for interpolation
+		var Pf1 = sub(Pf0, [1.0, 1.0, 1.0]); // Fractional part - 1.0
+
+		var ix = [Pi0[0], Pi1[0], Pi0[0], Pi1[0]];
+		var iy = [Pi0[1], Pi0[1], Pi1[1], Pi1[1]];
+		var iz0 = [Pi0[2], Pi0[2], Pi0[2], Pi0[2]];
+		var iz1 = [Pi1[2], Pi1[2], Pi1[2], Pi1[2]];
+
+		var ixy = permute(add(permute(ix), iy));
+		var ixy0 = permute(add(ixy, iz0));
+		var ixy1 = permute(add(ixy, iz1));
+
+		var gx0 = mult(ixy0, 1.0 / 7.0);
+		var gy0 = sub(fract(mult(floor(gx0), 1.0 / 7.0)), [0.5, 0.5, 0.5, 0.5]);
+		gx0 = fract(gx0);
+		var gz0 = sub(sub([0.5, 0.5, 0.5, 0.5], abs(gx0)), abs(gy0));
+		var sz0 = step(gz0, [0.0, 0.0, 0.0, 0.0]);
+		gx0 = sub(gx0, multiply(sz0, sub(step([0.0, 0.0, 0.0, 0.0], gx0), [0.5, 0.5, 0.5, 0.5])));
+		gy0 = sub(gy0, multiply(sz0, sub(step([0.0, 0.0, 0.0, 0.0], gy0), [0.5, 0.5, 0.5, 0.5])));
+
+		var gx1 = mult(ixy1, 1.0 / 7.0);
+		var gy1 = sub(fract(mult(floor(gx1), 1.0 / 7.0)), [0.5, 0.5, 0.5, 0.5]);
+		gx1 = fract(gx1);
+		var gz1 = sub(sub([0.5, 0.5, 0.5, 0.5], abs(gx1)), [0.5, 0.5, 0.5, 0.5]);
+		var sz1 = step(gz1, [0.0, 0.0, 0.0, 0.0]);
+		gx1 = sub(sz1, multiply(sz1, sub(step([0.0, 0.0, 0.0, 0.0], gx1), [0.5, 0.5, 0.5, 0.5])));
+		gy1 = sub(sz1, multiply(sz1, sub(step([0.0, 0.0, 0.0, 0.0], gy1), [0.5, 0.5, 0.5, 0.5])));
+
+		var g000 = [gx0[0], gy0[0], gz0[0]];
+		var g100 = [gx0[1], gy0[1], gz0[1]];
+		var g010 = [gx0[2], gy0[2], gz0[2]];
+		var g110 = [gx0[3], gy0[3], gz0[3]];
+		var g001 = [gx1[0], gy1[0], gz1[0]];
+		var g101 = [gx1[1], gy1[1], gz1[1]];
+		var g011 = [gx1[2], gy1[2], gz1[2]];
+		var g111 = [gx1[3], gy1[3], gz1[3]];
+
+		var norm0 = taylorInvSqrt([dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)]);
+		g000 = mult(g000, norm0[0]);
+		g010 = mult(g010, norm0[1]);
+		g100 = mult(g100, norm0[2]);
+		g110 = mult(g110, norm0[3]);
+		var norm1 = taylorInvSqrt([dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)]);
+		g001 = mult(g001, norm1[0]);
+		g011 = mult(g011, norm1[1]);
+		g101 = mult(g101, norm1[2]);
+		g111 = mult(g111, norm1[3]);
+
+		var n000 = dot(g000, Pf0);
+		var n100 = dot(g100, [Pf1[0], Pf0[1], Pf0[2]]);
+		var n010 = dot(g010, [Pf0[0], Pf1[1], Pf0[2]]);
+		var n110 = dot(g110, [Pf1[0], Pf1[1], Pf0[2]]);
+		var n001 = dot(g001, [Pf0[0], Pf0[1], Pf1[2]]);
+		var n101 = dot(g101, [Pf1[0], Pf0[1], Pf1[2]]);
+		var n011 = dot(g011, [Pf0[0], Pf1[1], Pf1[2]]);
+		var n111 = dot(g111, Pf1);
+
+		var fade_xyz = fade(Pf0);
+		var n_z = mix([n000, n100, n010, n110], [n001, n101, n011, n111], fade_xyz[2]);
+		var n_yz = mix([n_z[0], n_z[1]], [n_z[2], n_z[3]], fade_xyz[1]);
+		var n_xyz = mix([n_yz[0]], [n_yz[1]], fade_xyz[0]);
+
+		return 2.2 * n_xyz;
+	}
+
+	function turbulence(p) {
+		var w = 100.0;
+		var t = -0.5;
+		for (var f = 1.0 ; f <= 10.0 ; f++ ) {
+			var power = Math.pow( 2.0, f );
+			var power_p = mult(p, power)
+			t += Math.abs( pnoise(power_p, [10.0, 10.0, 10.0]) / power);
+		}
+		return t;
+	}
+
+	this.deformPoint = function(pos, normal) {
+		// Get a turbulent 3d noise using the normal, normal to high freq
+		var noise = 10.0 * -0.10 * turbulence(mult(normal, 0.5));
+	    // Get a 3d noise using the position, low frequency
+	    var b = 5.0 * pnoise(mult(pos, 0.05), [100.0, 100.0, 100.0]);
+	    // Compose both noises
+	    var displacement = - 10.0 * noise + b;
+	    var newPosition = add(pos, mult(normal, displacement));
+	    return newPosition;
+	}
+
+	var start = Date.now();
+	this.deformPointTime = function(pos, normal) {
+		var time = .00025 * ( Date.now() - start );
+		// Get a turbulent 3d noise using the normal, normal to high freq
+		var noise = 10.0 * -0.10 * turbulence(add(mult(normal, 0.5), [time, time, time]));
+	    // Get a 3d noise using the position, low frequency
+	    var b = 5.0 * pnoise(add(mult(pos, 0.05), mult([time, time, time], 2.0)), [200.0, 200.0, 200.0]);
+	    // Compose both noises
+	    var displacement = - noise + b;
+	    var newPosition = add(pos, mult(normal, displacement));
+	    return newPosition;
+	}
+
 }
