5abc95e7a958b2caf72cd6a36bd2b4b3c4d6bae3
vovakkk <vladimirkorukov@gmail.com>
2015-10-18 15:16:21 -0700

    Added autorunning

--- ttnn.js
+++ ttnn.js
@@ -8,9 +8,12 @@ var gamesDiv;
 
 var toColor = function(c) { return "rgb(" + (c >> 16) + "," + ((c >> 8) % 256) + "," + (c % 256) + ")"; }
 
+var runFast = false;
+
 // (ზ = function() {
 var oneStepMove = function() {
-	gamesDiv.innerHTML = "";
+	// if (!runFast)
+		gamesDiv.innerHTML = "";
 	// play a step
 	games.forEach(function(game, ind) {
 		if (victories[ind] == -1) {
@@ -34,7 +37,8 @@ var oneStepMove = function() {
 			}
 		}
 		game.switchPlayer();
-		gamesDiv.appendChild(generateCanvas(game, toColor(nns[ind * 2].color), toColor(nns[ind * 2 + 1].color)));
+		// if (!runFast)
+			gamesDiv.appendChild(generateCanvas(game, toColor(nns[ind * 2].color), toColor(nns[ind * 2 + 1].color)));
 	});
 	// if done
 	if (!victories.some(function(v) { return v == -1; })) {
@@ -62,49 +66,23 @@ var oneStepMove = function() {
 
 window.addEventListener("load", function() {
 	gamesDiv = document.querySelector("#games");
-	document.body.addEventListener("click", oneStepMove);
+	// document.body.addEventListener("click", oneStepMove);
+	var running = false;
+	document.querySelector("#continuous").addEventListener("click", function() {
+		running = !running;
+		if (running) {
+			var runTimeout = function() {
+				oneStepMove();
+				if (running)
+					setTimeout(runTimeout, runFast ? 10 : 300);	
+			};
+			runTimeout();
+		}
+		document.querySelector("#continuous").innerHTML = running ? "Stop" : "Run";
+	});
+	document.querySelector("#fast").addEventListener("change", function() {
+		runFast = document.querySelector("#fast").checked;
+	});
 });
 
-
-// Array.range(NUM_GAMES - 1).forEach(function() {
-// 	nns = nns.shuffle();
-// 	var games = Array.range(nns.length / 2).map(generateTictactoe);
-// 	var victories = games.map(function() { return 0; });
-// 	// play game
-// 	while (victories.some(function(v) { return v == 0; })) {
-// 		// play step for unfinished games
-// 		games.forEach(function(game, ind) {
-// 			if (victories[ind] == 0) {
-// 				var outp = runNetwork(nns[ind * 2 + (game.player % 2)], game.board.flatten().map(function(x) { return x == 0 ? 0.5 : (x == game.player) ? 0 : 1; }));
-// 				var bestVal = null;
-// 				var bestMove = null;
-// 				game.board.flatten().forEach(function(move, sqInd) {
-// 					if (move == game.EMPTY) {
-// 						if ((bestMove == null) || (bestVal < outp[sqInd])) {
-// 							bestMove = sqInd;
-// 							bestVal = outp[sqInd];
-// 						}
-// 					}
-// 				});
-// 				game.markSlot(Math.floor(bestMove / 3), bestMove % 3);
-// 				var victory = game.checkWinner();
-// 				if (victory) {
-// 					victories[ind] = victory % 2;
-// 				} else if (game.isFull()) {
-// 					victories[ind] = 1; // Arbitrary say 1 wins
-// 				}
-// 			}
-// 		});
-// 	}
-// 	// kill loosers
-// 	victories.forEach(function(vic, ind) {
-// 		nns.splice(ind + vic, 1);
-// 	});
-// });
-// // reproduce
-// nns = nns.map(function(surviver) {
-// 	return Array.range((1 << (NUM_GAMES - 1)) - 1).map(mutateNetwork.bind(null, surviver).concat([surviver]);
-// }).flatten();
-
 // ზ();
-// })();
